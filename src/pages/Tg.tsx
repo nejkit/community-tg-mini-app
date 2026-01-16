bb158eb08a80f4f6233ee29270c1d15a76bf32f8
Бачериков Никита <n.bacherikov@simcord.com>
2026-01-16 18:04:59 +0200

    fix

--- src/pages/Tg.tsx
+++ src/pages/Tg.tsx
@@ -1,13 +1,13 @@
 import WebApp from "@twa-dev/sdk";
 
 export default function initTelegram() {
-    console.log(WebApp)
+    const tg = WebApp;
 
-    WebApp.ready();
-    WebApp.expand();
+    tg.ready();
+    tg.expand();
 
     document.documentElement.style.setProperty(
         "--tg-theme-bg-color",
-        WebApp.themeParams.bg_color ?? "#fff"
+        tg.themeParams.bg_color ?? "#fff"
     );
 }
